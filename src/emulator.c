7d0eedbd3f4ec055367be8ad050c5787570f7cab
hoot-w00t <42416556+hoot-w00t@users.noreply.github.com>
2020-07-12 16:24:38 +0200

    Initialize all IO and CPU registers

--- src/emulator.c
+++ src/emulator.c
@@ -20,6 +20,7 @@ this program. If not, see <https://www.gnu.org/licenses/>.
 #include "gb/defs.h"
 #include "gb/memory.h"
 #include "gb/memory_banks.h"
+#include "gb/registers.h"
 #include "logger.h"
 #include <stdio.h>
 #include <stdlib.h>
@@ -48,8 +49,47 @@ gb_system_t *create_gb_system(void)
 
     memset(gb, 0, sizeof(gb_system_t));
 
+    // Initialize CPU registers
+    gb_register_write_u16(REG_AF, 0x01B0, gb);
+    gb_register_write_u16(REG_BC, 0x0013, gb);
+    gb_register_write_u16(REG_DE, 0x00D8, gb);
+    gb_register_write_u16(REG_HL, 0x014D, gb);
+
     gb->pc = CARTRIDGE_HEADER_LADDR;
     gb->sp = HRAM_UADDR;
+
+    // Initialize IO registers
+    gb_write_byte(0xFF05, 0x00, false, gb); // TIMA
+    gb_write_byte(0xFF06, 0x00, false, gb); // TMA
+    gb_write_byte(0xFF07, 0x00, false, gb); // TAC
+    gb_write_byte(0xFF10, 0x80, false, gb); // NR10
+    gb_write_byte(0xFF11, 0xBF, false, gb); // NR11
+    gb_write_byte(0xFF12, 0xF3, false, gb); // NR12
+    gb_write_byte(0xFF14, 0xBF, false, gb); // NR14
+    gb_write_byte(0xFF16, 0x3F, false, gb); // NR21
+    gb_write_byte(0xFF17, 0x00, false, gb); // NR22
+    gb_write_byte(0xFF19, 0xBF, false, gb); // NR24
+    gb_write_byte(0xFF1A, 0x7F, false, gb); // NR30
+    gb_write_byte(0xFF1B, 0xFF, false, gb); // NR31
+    gb_write_byte(0xFF1C, 0x9F, false, gb); // NR32
+    gb_write_byte(0xFF1E, 0xBF, false, gb); // NR33
+    gb_write_byte(0xFF20, 0xFF, false, gb); // NR41
+    gb_write_byte(0xFF21, 0x00, false, gb); // NR42
+    gb_write_byte(0xFF22, 0x00, false, gb); // NR43
+    gb_write_byte(0xFF23, 0xBF, false, gb); // NR30
+    gb_write_byte(0xFF24, 0x77, false, gb); // NR50
+    gb_write_byte(0xFF25, 0xF3, false, gb); // NR51
+    gb_write_byte(0xFF26, 0xF1, false, gb); // NR52
+    gb_write_byte(0xFF40, 0x91, false, gb); // LCDC
+    gb_write_byte(0xFF42, 0x00, false, gb); // SCY
+    gb_write_byte(0xFF43, 0x00, false, gb); // SCX
+    gb_write_byte(0xFF45, 0x00, false, gb); // LYC
+    gb_write_byte(0xFF47, 0xFC, false, gb); // BGP
+    gb_write_byte(0xFF48, 0xFF, false, gb); // OBP0
+    gb_write_byte(0xFF49, 0xFF, false, gb); // OBP1
+    gb_write_byte(0xFF4A, 0x00, false, gb); // WY
+    gb_write_byte(0xFF4B, 0x00, false, gb); // WX
+    gb_write_byte(0xFFFF, 0x00, false, gb); // IE
 
     return gb;
 }
