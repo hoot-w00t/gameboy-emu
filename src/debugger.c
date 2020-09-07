71d1e11b80f4b39551500ac7886dc4424b56777a
hoot-w00t <42416556+hoot-w00t@users.noreply.github.com>
2020-09-07 21:09:04 +0200

    Add breakpoints to debugger

--- src/debugger.c
+++ src/debugger.c
@@ -21,13 +21,21 @@ this program. If not, see <https://www.gnu.org/licenses/>.
 #include "gb/memory.h"
 #include "gb/cartridge.h"
 #include <stdio.h>
+#include <stddef.h>
 #include <stdlib.h>
 #include <readline/readline.h>
 #include <readline/history.h>
 
+struct breakpoint {
+    bool pc_break;
+    uint16_t pc;
+    struct breakpoint *next;
+};
+
 static struct params {
     int last_ret;
     bool emulate_cycles;
+    struct breakpoint *bps;
 } params;
 
 static inline bool empty_line(const char *line)
@@ -46,6 +54,96 @@ void emulate_n_cycles(gb_system_t *gb, uint32_t n)
     }
 }
 
+void emulate_until_breakpoint(gb_system_t *gb)
+{
+    struct breakpoint *bp;
+    bool run = true;
+
+    while (run && (params.last_ret = gb_cpu_cycle(gb, params.emulate_cycles)) >= 0) {
+        for (bp = params.bps; bp; bp = bp->next) {
+            if (bp->pc_break && bp->pc == gb->pc) {
+                run = false;
+                printf("Breakpoint: PC at 0x%04X\n", bp->pc);
+            }
+        }
+    }
+}
+
+void bp_add_pc(uint16_t pc)
+{
+    struct breakpoint *bp = malloc(sizeof(struct breakpoint));
+
+    if (!bp) {
+        fprintf(stderr, "Memory allocation failed\n");
+        return;
+    }
+
+    memset(bp, 0, sizeof(struct breakpoint));
+    bp->pc = pc;
+    bp->pc_break = true;
+
+    if (params.bps) {
+        struct breakpoint *last = params.bps;
+
+        while (last->next) {
+            if (last->pc_break && last->pc == pc) {
+                free(bp);
+                fprintf(stderr, "A breakpoint for PC=0x%04X already exists\n", pc);
+                return;
+            }
+            last = last->next;
+        }
+        if (last->pc_break && last->pc == pc) {
+            free(bp);
+            fprintf(stderr, "A breakpoint for PC=0x%04X already exists\n", pc);
+            return;
+        }
+
+        last->next = bp;
+    } else {
+        params.bps = bp;
+    }
+    printf("Added breakpoint for PC=0x%04X\n", pc);
+}
+
+void bp_remove_pc(uint16_t pc)
+{
+    struct breakpoint *prev = NULL;
+    struct breakpoint *bp;
+
+    for (bp = params.bps; bp; bp = bp->next) {
+        if (bp->pc_break && bp->pc == pc) {
+            if (prev) {
+                prev->next = bp->next;
+                free(bp);
+            } else {
+                params.bps = bp->next;
+                free(bp);
+            }
+            printf("Removed breakpoint for PC=0x%04X\n", pc);
+            return;
+        }
+
+        prev = bp;
+    }
+
+    fprintf(stderr, "No breakpoint for PC=0x%04X\n", pc);
+}
+
+void bp_freeall(void)
+{
+    struct breakpoint *bp = params.bps;
+    struct breakpoint *next;
+
+    while (bp) {
+        next = bp->next;
+        free(bp);
+        bp = next;
+    }
+
+    params.bps = NULL;
+}
+
 void debug_gb_system(gb_system_t *gb)
 {
     const char tokdelim[] = " ";
@@ -55,6 +153,7 @@ void debug_gb_system(gb_system_t *gb)
     // Default values
     params.last_ret = 0;
     params.emulate_cycles = false;
+    params.bps = NULL;
 
     stifle_history(50);
     while (1) {
@@ -97,6 +196,29 @@ void debug_gb_system(gb_system_t *gb)
             emulate_n_cycles(gb, 1);
             dump_cpu(gb);
 
+        } else if (!strcmp(tok, "bp") || !strcmp(tok, "breakpoint")) {
+            // Add/remove breakpoints
+            if (!(tok = strtok(NULL, tokdelim))) {
+                printf("Breakpoints:\n");
+                for (struct breakpoint *bp = params.bps; bp; bp = bp->next) {
+                    if (bp->pc_break) {
+                        printf("PC=0x%04X\n", bp->pc);
+                    } else {
+                        printf("Unexpected empty breakpoint\n");
+                    }
+                }
+                continue;
+            }
+
+            if (!strcmp(tok, "add")) {
+                bp_add_pc((uint16_t) strtol(strtok(NULL, tokdelim), NULL, 16));
+            } else if (!strcmp(tok, "remove")) {
+                bp_remove_pc((uint16_t) strtol(strtok(NULL, tokdelim), NULL, 16));
+            }
+
+        } else if (!strcmp(tok, "run")) {
+            emulate_until_breakpoint(gb);
+
         } else if (!strcmp(tok, "cpu")) {
             // cpu
             // Dump CPU
@@ -131,5 +253,6 @@ void debug_gb_system(gb_system_t *gb)
     }
 
     free(line);
+    bp_freeall();
     printf("Exitting debugger\n");
 }
