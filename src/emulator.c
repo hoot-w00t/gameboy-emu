ef4b1d019af27022dc1fb48393d6eeb1a01fe311
hoot-w00t <42416556+hoot-w00t@users.noreply.github.com>
2020-09-07 19:33:18 +0200

    Rewrite CLI interface

--- src/emulator.c
+++ src/emulator.c
@@ -17,130 +17,92 @@ You should have received a copy of the GNU General Public License along with
 this program. If not, see <https://www.gnu.org/licenses/>.
 */
 
-#include "gb/defs.h"
-#include "gb/memory.h"
-#include "gb/memory_banks.h"
-#include "gb/registers.h"
-#include "gb/cpu.h"
-#include "gb/opcodes.h"
+#include "gb/system.h"
 #include "logger.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <stddef.h>
-#include <string.h>
 #include <getopt.h>
 
-// Free gb_system_t
-// If include_ptr is true, also free *gb
-void free_gb_system(gb_system_t *gb, bool include_ptr)
-{
-    gb_free_membank(&gb->memory.rom_banks);
-    gb_free_membank(&gb->memory.ram_banks);
+static struct args {
+    bool debug;
+    char *filename;
+} args;
 
-    if (include_ptr)
-        free(gb);
+void print_usage(const char *cmd)
+{
+    printf("Usage: %s [-d] [-l level] filename\n", cmd);
 }
 
-// Allocate and initialize gb_system_t
-gb_system_t *create_gb_system(void)
+void print_help(const char *cmd)
 {
-    gb_system_t *gb = malloc(sizeof(gb_system_t));
-
-    if (!gb)
-        return NULL;
-
-    memset(gb, 0, sizeof(gb_system_t));
-
-    // Initialize CPU registers
-    gb_register_write_u16(REG_AF, 0x01B0, gb);
-    gb_register_write_u16(REG_BC, 0x0013, gb);
-    gb_register_write_u16(REG_DE, 0x00D8, gb);
-    gb_register_write_u16(REG_HL, 0x014D, gb);
-
-    gb->pc = CARTRIDGE_HEADER_LADDR;
-    gb->sp = HRAM_UADDR;
-
-    // Initialize IO registers
-    gb_write_byte(0xFF05, 0x00, false, gb); // TIMA
-    gb_write_byte(0xFF06, 0x00, false, gb); // TMA
-    gb_write_byte(0xFF07, 0x00, false, gb); // TAC
-    gb_write_byte(0xFF10, 0x80, false, gb); // NR10
-    gb_write_byte(0xFF11, 0xBF, false, gb); // NR11
-    gb_write_byte(0xFF12, 0xF3, false, gb); // NR12
-    gb_write_byte(0xFF14, 0xBF, false, gb); // NR14
-    gb_write_byte(0xFF16, 0x3F, false, gb); // NR21
-    gb_write_byte(0xFF17, 0x00, false, gb); // NR22
-    gb_write_byte(0xFF19, 0xBF, false, gb); // NR24
-    gb_write_byte(0xFF1A, 0x7F, false, gb); // NR30
-    gb_write_byte(0xFF1B, 0xFF, false, gb); // NR31
-    gb_write_byte(0xFF1C, 0x9F, false, gb); // NR32
-    gb_write_byte(0xFF1E, 0xBF, false, gb); // NR33
-    gb_write_byte(0xFF20, 0xFF, false, gb); // NR41
-    gb_write_byte(0xFF21, 0x00, false, gb); // NR42
-    gb_write_byte(0xFF22, 0x00, false, gb); // NR43
-    gb_write_byte(0xFF23, 0xBF, false, gb); // NR30
-    gb_write_byte(0xFF24, 0x77, false, gb); // NR50
-    gb_write_byte(0xFF25, 0xF3, false, gb); // NR51
-    gb_write_byte(0xFF26, 0xF1, false, gb); // NR52
-    gb_write_byte(0xFF40, 0x91, false, gb); // LCDC
-    gb_write_byte(0xFF42, 0x00, false, gb); // SCY
-    gb_write_byte(0xFF43, 0x00, false, gb); // SCX
-    gb_write_byte(0xFF45, 0x00, false, gb); // LYC
-    gb_write_byte(0xFF47, 0xFC, false, gb); // BGP
-    gb_write_byte(0xFF48, 0xFF, false, gb); // OBP0
-    gb_write_byte(0xFF49, 0xFF, false, gb); // OBP1
-    gb_write_byte(0xFF4A, 0x00, false, gb); // WY
-    gb_write_byte(0xFF4B, 0x00, false, gb); // WX
-    gb_write_byte(0xFFFF, 0x00, false, gb); // IE
-
-    return gb;
+    print_usage(cmd);
+    printf("\nDescription:\n");
+    printf("    filename        GameBoy ROM to emulate\n\n");
+    printf("    -h              Show this help message\n");
+    printf("    -l level        Set logging to level (default: warn)\n");
+    printf("                    Options: crit, error, warn, info, debug, all\n");
+    printf("    -d              Run in debugging mode\n");
 }
 
-void parse_args(int ac, char **av, char **filename)
+void parse_args(int ac, char **av)
 {
-    const char shortopts[] = "l:";
+    const char shortopts[] = "hl:d";
     int opt;
 
+    // Default values
+    args.debug = false;
+    args.filename = NULL;
+
+    // Optionnal arguments
     while ((opt = getopt(ac, av, shortopts)) >= 0) {
         switch (opt) {
+            case 'h':
+                print_help(av[0]);
+                exit(EXIT_SUCCESS);
+
             case 'l':
                 if (set_logger_level(optarg) < 0) {
                     fprintf(stderr, "Invalid log level\n");
                     exit(EXIT_FAILURE);
                 }
                 break;
+
+            case 'd':
+                args.debug = true;
+                break;
+
             default: exit(EXIT_FAILURE);
         }
     }
 
+    // Positionnal arguments
     if (optind >= ac) {
-        fprintf(stderr, "Missing positionnal argument: filename\n");
+        fprintf(stderr, "Missing argument: filename\n");
         exit(EXIT_FAILURE);
     }
-
-    *filename = av[optind];
+    args.filename = av[optind];
 }
 
 int main(int ac, char **av)
 {
     gb_system_t *gb;
-    char *filename;
+    int status;
 
-    parse_args(ac, av, &filename);
-    if ((gb = create_gb_system()) == NULL) {
+    parse_args(ac, av);
+    if (!(gb = create_gb_system())) {
         fprintf(stderr, "Memory allocation failed");
         return EXIT_FAILURE;
     }
 
-    if (gb_load_rom(filename, gb) < 0)
+    if (load_rom_from_file(args.filename, gb) < 0)
         return EXIT_FAILURE;
 
-    int last_status;
-
-    while ((last_status = gb_cpu_cycle(gb)) >= 0) {
-        continue;
+    if (args.debug) {
+        printf("Todo: Add debug mode\n");
+    } else {
+        status = emulate_gb_system(gb);
     }
 
     free_gb_system(gb, true);
-    return 0;
+    return status >= 0 ? EXIT_SUCCESS : EXIT_FAILURE;
 }
